ed832342d94b3dd8772fd78f6cb61682d06d9e94
何天速 <hetiansu@qudian.com>
2019-05-17 15:10:20 +0800

    base model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -15,22 +15,24 @@ class BaseModel extends Model
      * 获取单个ID记录
      *
      * @param int $id
+     * @param array $columns
      * @return mixed
      */
-    public static function getOne($id)
+    public static function getOne($id, $columns = ["*"])
     {
-        return static::query()->where('id', $id)->first();
+        return static::query()->find($id, $columns);
     }
 
     /**
      * 获取多个ID记录
      *
      * @param array $idArr
+     * @param array $columns
      * @return mixed
      */
-    public static function getMulti($idArr)
+    public static function getMulti($idArr, $columns = ["*"])
     {
-        return static::query()->whereIn('id', $idArr)->get();
+        return static::query()->whereIn('id', $idArr)->get($columns);
     }
 
 }
